6abb79e3a99b352bc264f0591ab42818ca826e86
James <150948866+jameslaight@users.noreply.github.com>
2024-04-20 15:30:50 +0100

    whitespace

--- core/src/com/monjaro/gamejam/Actor.java
+++ core/src/com/monjaro/gamejam/Actor.java
@@ -4,6 +4,7 @@ package com.monjaro.gamejam;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public abstract class Actor {
+
 	public abstract void tick();
 
 	public abstract void render(SpriteBatch batch);
