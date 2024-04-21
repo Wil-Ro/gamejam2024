c099ac0c5fdcb91d016a0385b84135a832b79b1d
Rosia E Evans <willowro@outlook.com>
2024-04-21 01:29:38 +0100

    adds textures

---
+++ core/src/com/monjaro/gamejam/UI.java
@@ -0,0 +1,16 @@
+package com.monjaro.gamejam;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class UI extends Actor{
+    Transform transform
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+}
